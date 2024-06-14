9f9b83a49938e402eba1934b182c77a6193d5312
Vaibhavi <vaibhavichaudhari304@gmail.com>
2024-06-14 22:03:46 +0530

    added new theme feature

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// let display = document.querySelector(".display");
+
+// function appendToDisplay(inputValue) {
+//     display.value += inputValue;
+// }
+
+// function backSpace() {
+//     display.value = display.value.slice(0, -1);
+// }
+
+// function clearDisplay() {
+//     display.value = "";
+// }
+
+// function calculate() {
+//     display.value = eval(display.value);
+// }
+
 let display = document.querySelector(".display");
 
 function appendToDisplay(inputValue) {
@@ -14,4 +32,8 @@ function clearDisplay() {
 
 function calculate() {
     display.value = eval(display.value);
+}
+
+function changeTheme(theme) {
+    document.getElementById('theme-link').href = theme;
 }
